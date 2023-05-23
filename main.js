19a92668c66080f4648f5a8694f0b94d2699eca0
Natalie <86774243+nnatalieh@users.noreply.github.com>
2023-05-22 21:25:37 -0500

    develop all files

---
+++ main.js
@@ -0,0 +1,24 @@
+const gridSizeRange = document.querySelector('#slider');
+const gridSizeOutputP = document.querySelector('#output');
+const gridDiv = document.querySelector('.grid-container');
+
+function grid(size=16) {
+    gridDiv.style.setProperty('--grid-rows', size);
+    gridDiv.style.setProperty('--grid-cols', size);
+    for (i = 0; i < size ** 2; i++) {
+        let div = document.createElement('div');
+        gridDiv.appendChild(div).className = "grid-item";
+    };
+};
+
+function clear(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+gridSizeRange.addEventListener('click', function(e) {
+    clear(gridDiv);
+    gridSizeOutputP.innerText = gridSizeRange.value + ' x ' + gridSizeRange.value;
+    grid(gridSizeRange.value);
+});
