2c4fb48dc167fbffcd9a43b69bdee71bb20d48f8
Natalie <86774243+nnatalieh@users.noreply.github.com>
2023-05-23 02:45:09 -0500

    add buttons and edit layout

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const gridSizeRange = document.querySelector('#slider');
 const gridSizeOutputP = document.querySelector('#output');
 const gridDiv = document.querySelector('.grid-container');
-const gridItemsDiv = document.querySelector('.grid-item');
+const resetButton = document.querySelector('#reset');
+const changeColorButton = document.querySelector('#change-color');
 
 function grid(size=16) {
     gridDiv.style.setProperty('--grid-rows', size);
@@ -10,6 +11,7 @@ function grid(size=16) {
         let div = document.createElement('div');
         gridDiv.appendChild(div).className = "grid-item";
         div.addEventListener('mouseover', function(e) {
+            div.id = "filled";
             e.target.style.backgroundColor = 'black';
         });
     };
@@ -19,12 +21,26 @@ function clear(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     };
+    grid(gridSizeRange.value);
 };
 
 gridSizeRange.addEventListener('click', function(e) {
     clear(gridDiv);
     gridSizeOutputP.innerText = gridSizeRange.value + ' x ' + gridSizeRange.value;
-    grid(gridSizeRange.value);
+});
+
+resetButton.addEventListener('click', function(e) {
+    clear(gridDiv);
+});
+
+changeColorButton.addEventListener('click', function(e) {
+    const filledDiv = document.querySelectorAll('#filled');
+    filledDiv.forEach(filledDiv => {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        filledDiv.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    });
 });
 
 grid()
